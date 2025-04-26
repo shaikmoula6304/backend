2e945a77146c5ab0998aef9a0bc052c4283b08cf
shaikmoula6304 <162301943+shaikmoula6304@users.noreply.github.com>
2025-04-26 12:48:29 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import json
+import os
 
-# Load service account credentials
-cred = credentials.Certificate("serviceAccountKey.json")
+# Load Firebase credentials from environment variable
+firebase_cred_json = os.getenv('FIREBASE_CREDENTIAL')
 
-# Initialize Firebase Admin SDK with Realtime Database
+# Convert the JSON string into a dictionary
+cred_dict = json.loads(firebase_cred_json)
+
+# Create credentials object
+cred = credentials.Certificate(cred_dict)
+
+# Initialize Firebase Admin SDK
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://blog-f8552-default-rtdb.firebaseio.com/'  # Replace with your DB URL
+    'databaseURL': os.getenv('FIREBASE_DATABASE_URL')  # Get DB URL from environment variables
 })
